2efbf0051db5af47c6e25b1f5c3afa1efcb5414b
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-04-01 19:57:01 +0200

    Add files via upload

--- fracture.py
+++ fracture.py
@@ -15,16 +15,34 @@ class Fracture():
 
         #alpha, teta e phi sono usati analogamente al pdf della descrizione del progetto
 
+        #n = numero di vertici
         self.n = n
+
+        #rx = lunghezza semiasse ascisse
         self.rx = rx
+
+        #ry = lunghezza semiasse ordinate
         self.ry = rx / ar
+
+        #alpha = angolo di rotazione attorno all'asse z (prima rotazione)
         self.alpha = alpha
-        self.teta = mc.cart2sph(v[0], v[1], v[2])[0]
+
+        #phi = elevation (seconda rotazione)
         self.phi = mc.cart2sph(v[0], v[1], v[2])[1]
+
+        #teta = azimuth (terza rotazione)
+        self.teta = mc.cart2sph(v[0], v[1], v[2])[0]
+
+        #vn = versore normale
         self.vn = v
+
+        #bar = baricentro
+        self.bar = bar
+
+        #vertici = matrice 3xn contenente i vertici come vettore colonna ordinati in senso antiorario come richiesto
         self.vertici = np.zeros((3, n))     #prealloco la matrice dei vertici perché ne conosco le dimensioni
 
-        # ciclo for per generare i vertici del poligono
+        #ciclo for per generare i vertici del poligono
         for i in range(n):
             self.vertici[0, i] = self.rx * np.cos(i * 2 * np.pi / n)
             self.vertici[1, i] = self.ry * np.sin(i * 2 * np.pi / n)
@@ -46,7 +64,6 @@ class Fracture():
         self.zmin = np.min(self.vertici[2, :])
         self.zmax = np.max(self.vertici[2, :])
 
-        #il Bounding Box è meglio salvarlo così con 6 variabili o direttamente in una matrice???
 
 
 
