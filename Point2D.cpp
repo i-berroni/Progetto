81bfea310f663fbaf8246b28cd9c22c85b4ab3be
AngeloCodes <48890525+AngeloCodes@users.noreply.github.com>
2019-05-03 16:22:50 +0200

    Update Point2D.cpp

--- Point2D.cpp
+++ Point2D.cpp
@@ -11,25 +11,30 @@ float Point2D::getx(){
 float Point2D::gety(){
             return y;
         }
-float Point2D::norm(){        
+float Point2D::norm(){
             return sqrt(x*x+y*y);
     }
 Point2D::Point2D(float i)         // il costruttore deve avere lo stesso nome della classe
         {
-            
+
                     x=i;
                     y=i;
-        }    
+        }
 Point2D::Point2D(float i, float j)// il costruttore deve avere lo stesso nome della classe
         {
-            
+
                     x=i;
                     y=j;
-        } 
+        }
+Point2D::Point2D()
+{
+    x=0;
+    y=0;
+}
 Point2D::~Point2D()// il distruttore non vuole parametri
         {
             cout <<"\nDestructor called\n";
-        } 
+        }
 Point2D::Point2D(const Point2D &point){
 x=point.x;
 y=point.y;
@@ -47,7 +52,7 @@ Point2D Point2D::operator- (Point2D &a)
 }
 
 main(){
-float norma, a, b, c, d; 
+float norma, a, b, c, d;
 Point2D somm;
 Point2D diff;
 cout <<"Immetti i valori dell'oggetto 1 che vuoi creare e premi invio\n";
@@ -67,13 +72,21 @@ m.show();
 Point2D n=m; //serve a copiare il contenuto dell'oggetto m in un nuovo oggetto n con gli stessi attributi
 cout <<"\nQuesto e' l'oggetto copiato con il costruttore di copia:\n";
 n.show();
-cout <<"\nQuesta e' la norma:\n";
+
+cout <<"\nQuesta e' la norma del primo oggetto:\n";
 norma=n.norm();
-cout << norma;
+cout <<norma;
+
+cout <<"\nQuesta e' la norma del secondo oggetto:\n";
+norma=f.norm();
+cout <<norma;
+
+
 cout <<"\nQuesta e' la somma:\n";
 somm=m+f;
 diff=m-f;
-
+somm.show();
+diff.show();
 
 
 
@@ -81,9 +94,6 @@ return 0;
 
 
 };
-
-
-
 
 
 
