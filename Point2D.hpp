b23ce2fee848c91a93d6b4e85f4f0361abe432b5
AngeloCodes <48890525+AngeloCodes@users.noreply.github.com>
2019-05-03 16:23:25 +0200

    Update Point2D.hpp

--- Point2D.hpp
+++ Point2D.hpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+
 using namespace std;
 
 class Point2D{
-        private: 
+        private:
             float x, y;
-            
+
         public:
-        
-        Point2D(float i);
-        Point2D(float i , float j);
-        ~Point2D(); 
+
+        Point2D(float );
+        Point2D(float, float);
+        Point2D();
+        ~Point2D();
         Point2D(const Point2D &);
     Point2D operator+(Point2D &);
     Point2D operator-(Point2D &);
@@ -20,3 +22,4 @@ class Point2D{
     float gety();
     float norm();
 };
+
