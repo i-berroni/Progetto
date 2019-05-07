981a9418ffea121c11f65c9b670e8cb1a74a408b
AngeloCodes <48890525+AngeloCodes@users.noreply.github.com>
2019-05-07 18:55:30 +0200

    Update differenza_divisa.m

--- differenza_divisa.m
+++ differenza_divisa.m
@@ -8,106 +8,59 @@ h=10^-7;
 %scrivo la vera funzione e la sua derivata:
 f=@(x) x.^3;
 fder=@(x) 3.*(x.^2);
-%scivo la sua differenza divisa
-fd=@(x) (f(x+h)-f(x))./h;
-%Il valore della differenza divisa
-%nel punto x0 è dato da fd=fd(x0);
-
+f_diffdivisa = @(x) ((x+h).^3 - x.^3)./h;
 
 %PUNTO a:
 %riscrivo la differenza finita
 %usando la differenza tra cubi (si fa in carta e penna):
-f_riscritta=h^2;
+f_riscritta=@(x) 3.*x.^2+3.*x.*h+h.^2;
+
+%Per x^5:
+% %scrivo la vera funzione e la sua derivata:
+% f=@(x) x.^5;
+% fder=@(x) 5.*(x.^4);
+% f_diffdivisa = @(x) ((x-h).^5 - x.^5)./h
+% 
+% %PUNTO a:
+% %riscrivo la differenza finita
+% %usando la differenza tra cubi (si fa in carta e penna):
+% f_riscritta=@(x) 10.*x.^3.*h+10.*x.^2.*h^2+5.*x.*h^3+h.^4+5.*x.^4;
+
 
 %PUNTO b:
 x=10.^(1:4);
-for i=x
-%valori effettivi della funzione(facoltativo)
-y=f(i);
-%valori della differenza divisa non semplificata
-yd=fd(i);
-end
+y_esatta = fder(x);
+y_diffdivisa = f_diffdivisa(x);
+y_riscritta = f_riscritta(x);
 
+%prealloco gli errori:
+err_ass_diffdiv=zeros(length(x),1);
+err_rel_diffdiv=zeros(length(x),1);
+err_ass_risc=zeros(length(x),1);
+err_rel_risc=zeros(length(x),1);
 
+%calcolo gli errori di y_diffdivisa
+err_ass_diffdiv(1:4)=abs(y_diffdivisa(1:4) - y_esatta(1:4));
+err_rel_diffdiv(1:4)=abs(y_diffdivisa(1:4) - y_esatta(1:4))./abs(y_esatta(1:4));
 
-
-%calcolo l'errore assoluto:
-err_ass=zeros(length(x));
-i=1;
-for j=x
-    err_ass(i)=abs(fd(j)-fder(j));
-    i=i+1;
-end
-
-%calcolo l'errore relativo:
-err_rel=zeros(length(x));
-i=1;
-for j=x
-    err_rel(i)=abs(fd(j)-fder(j))/abs(fder(j));
-    i=i+1;
-end
+%calcolo gli errori di y_riscritta
+err_ass_risc(1:4)=abs(y_riscritta(1:4) - y_esatta(1:4));
+err_rel_risc(1:4)=abs(y_riscritta(1:4) - y_esatta(1:4))./abs(y_esatta(1:4));
 
 %li plotto con questi formati:
 %crea due figure, li visualizza una sopra l'altra quindi spostale.
+%plotto riscritte:
+% figure;
+% semilogy(x,err_ass_risc,'-o','MarkerIndices',1:length(x),'MarkerEdgeColor','r');
+
+%  figure;
+%  semilogy(x,err_rel_risc,'b--o','MarkerIndices',1:length(x));
+% 
+%Plotto differenze divise:
+
 figure;
-loglog(x,err_ass,'-o','MarkerIndices',1:length(x),'MarkerEdgeColor','r');
-figure;
-loglog(x,err_ass,'b--o','MarkerIndices',1:length(x));
+loglog(x,err_ass_diffdiv,'b--o','MarkerIndices',1:length(x));
+% 
+% figure;
+% semilogy(x,err_rel_diffdiv,'b--o','MarkerIndices',1:length(x));
 
-%% RIPETO CON LA FUNZIONE y=x.^5
-
-%esercizio_8
-clear all
-close all
-clc
-format long e
-%fisso h:
-h=10^-7;
-%scrivo la vera funzione e la sua derivata:
-f=@(x) x.^5;
-fder=@(x) 5.*(x.^4);
-%scivo la sua differenza divisa
-fd=@(x) (f(x+h)-f(x))./h;
-%Il valore della differenza divisa
-%nel punto x0 è dato da fd=fd(x0);
-
-
-%PUNTO a:
-%riscrivo la differenza finita (si fa in carta e penna)
-% x5 - a5 = (x-a) (x4 +ax3 +a2x2 +a3x +a4) :
-f_riscritta=%calcolala a mano facendo i conti, mi secca haha;
-
-%PUNTO b:
-x=10.^(1:4);
-for i=x
-%valori effettivi della funzione(facoltativo)
-y=f(i);
-%valori della differenza divisa non semplificata
-yd=fd(i);
-end
-
-
-
-
-%calcolo l'errore assoluto:
-err_ass=zeros(length(x));
-i=1;
-for j=x
-    err_ass(i)=abs(fd(j)-fder(j));
-    i=i+1;
-end
-
-%calcolo l'errore relativo:
-err_rel=zeros(length(x));
-i=1;
-for j=x
-    err_rel(i)=abs(fd(j)-fder(j))/abs(fder(j));
-    i=i+1;
-end
-
-%li plotto con questi formati:
-%crea due figure, li visualizza una sopra l'altra quindi spostale.
-figure;
-loglog(x,err_ass,'-o','MarkerIndices',1:length(x),'MarkerEdgeColor','r');
-figure;
-loglog(x,err_ass,'b--o','MarkerIndices',1:length(x));
