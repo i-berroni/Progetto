0b9842db06e7569cd1896bc99b4205a168c541de
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-05-17 16:49:48 +0200

    Update esercizio_pcs.py

--- esercizio_pcs.py
+++ esercizio_pcs.py
@@ -1,25 +1,30 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Che colori usare? Dovrebbe servire solo un colore per "scoperto" (verde) e uno per "non scoperto" (blu)
 
-# NON FUNZIONA IL PLOT DEI GRAFI QUANDO I PARAMETRI SONO SETTATI COME TRUE, NONOSTANTE IL COMANDO SIA CORRETTO
-
-# E' DA IMPLEMENTARE ANCORA LA POSSIBILITA' DI VEDERE UN'ANIMAZIONE DELLA VISITA DEL GRAFO
-
-
-def visita_profondita(grafo, vertice_start, plot_grafo=False, plot_albero=False):
+def visita_profondita(grafo, vertice_start, plot_grafo=False, plot_albero=False, plot_anim=False):
     """
     Visita in profondita' di un grafo con albero di ricoprimento
 
     :param grafo: deve essere un grafo di networkx
     :param vertice_start: nodo arbitrario del grafo
+    :param plot_grafo: booleano che indica se visualizzare il grafo da visitare
+    :param plot_albero: booleano che indica se visualizzare l'albero di ricoprimento del grafo
+    :param plot_anim: booleano che indica se visualizzare l'animazione della visita del grafo
     :return: albero di ricoprimento della visita
     """
 
     albero = nx.Graph()
     nodi_da_visitare = []
     nodi_da_visitare.append(vertice_start)
+
+    pos = nx.spring_layout(grafo)
+
+    # Le due liste "nodi_scoperti" e "nodi_non_scoperti" servono per l'animazione della visita
+    if plot_anim is True:
+        nodi_scoperti = [vertice_start]
+        nodi_non_scoperti = list(grafo.nodes())
+        nodi_non_scoperti.remove(vertice_start)
 
     for v in grafo.nodes():
         if v != vertice_start:
@@ -28,34 +33,64 @@ def visita_profondita(grafo, vertice_start, plot_grafo=False, plot_albero=False)
     grafo.nodes[vertice_start]['colore'] = 'verde'  # verde -> scoperto
     albero.add_node(vertice_start)
 
+    plt.axes().clear()
     while len(nodi_da_visitare) > 0:
+
         v_green = nodi_da_visitare.pop()  # la mia struttura dati e' una pila: rimuovo l'ultimo elemento inserito
+
         for v in list(grafo[v_green]):
+
+            if plot_anim is True:
+                nx.draw_networkx(grafo, pos, nodelist=nodi_non_scoperti, node_color='b')
+                nx.draw_networkx(grafo, pos, nodelist=nodi_scoperti, node_color='g')
+                plt.pause(0.2)
+                plt.axes().clear()
+
             if grafo.nodes[v]['colore'] != 'verde':
                 nodi_da_visitare.append(v)
                 grafo.nodes[v]['colore'] = 'verde'
                 albero.add_node(v)
                 albero.add_edge(v_green, v)
+                if plot_anim is True:
+                    nodi_non_scoperti.remove(v)
+                    nodi_scoperti.append(v)
 
     if plot_grafo is True:
-        nx.draw_networkx(grafo)
+        nx.draw_networkx(grafo, pos)
+        plt.pause(2)
+        plt.axes().clear()
+
     if plot_albero is True:
-        nx.draw_networkx(albero)
+        nx.draw_networkx(albero, pos)
+        plt.pause(2)
+        plt.axes().clear()
 
     return albero
 
-def visita_ampiezza(grafo, vertice_start, plot_grafo=False, plot_albero=False):
+
+def visita_ampiezza(grafo, vertice_start, plot_grafo=False, plot_albero=False, plot_anim=False):
     """
-    Visita in profondita' di un grafo con albero di ricoprimento
+    Visita in ampiezza di un grafo con albero di ricoprimento
 
     :param grafo: deve essere un grafo di networkx
     :param vertice_start: nodo arbitrario del grafo
+    :param plot_grafo: booleano che indica se visualizzare il grafo da visitare
+    :param plot_albero: booleano che indica se visualizzare l'albero di ricoprimento del grafo
+    :param plot_anim: booleano che indica se visualizzare l'animazione della visita del grafo
     :return: albero di ricoprimento della visita
     """
 
     albero = nx.Graph()
     nodi_da_visitare = []
     nodi_da_visitare.append(vertice_start)
+
+    pos = nx.spring_layout(grafo)
+
+    # Le due liste "nodi_scoperti" e "nodi_non_scoperti" servono per l'animazione della visita
+    if plot_anim is True:
+        nodi_scoperti = [vertice_start]
+        nodi_non_scoperti = list(grafo.nodes())
+        nodi_non_scoperti.remove(vertice_start)
 
     for v in grafo.nodes():
         if v != vertice_start:
@@ -64,32 +99,36 @@ def visita_ampiezza(grafo, vertice_start, plot_grafo=False, plot_albero=False):
     grafo.nodes[vertice_start]['colore'] = 'verde'  # verde -> scoperto
     albero.add_node(vertice_start)
 
+    plt.axes().clear()
     while len(nodi_da_visitare) > 0:
-        v_green = nodi_da_visitare.pop(0)  # la mia struttura dati e' una coda: rimuovo il primo elemento inserito (rimasto nella coda)
+
+        v_green = nodi_da_visitare.pop(0)  # la mia struttura dati e' una coda: rimuovo il primo elemento inserito
+
         for v in list(grafo[v_green]):
+
+            if plot_anim is True:
+                nx.draw_networkx(grafo, pos, nodelist=nodi_non_scoperti, node_color='b')
+                nx.draw_networkx(grafo, pos, nodelist=nodi_scoperti, node_color='g')
+                plt.pause(0.2)
+                plt.axes().clear()
+
             if grafo.nodes[v]['colore'] != 'verde':
                 nodi_da_visitare.append(v)
                 grafo.nodes[v]['colore'] = 'verde'
                 albero.add_node(v)
                 albero.add_edge(v_green, v)
+                if plot_anim is True:
+                    nodi_non_scoperti.remove(v)
+                    nodi_scoperti.append(v)
 
     if plot_grafo is True:
-        nx.draw_networkx(grafo)
+        nx.draw_networkx(grafo, pos)
+        plt.pause(2)
+        plt.axes().clear()
+
     if plot_albero is True:
-        nx.draw_networkx(albero)
+        nx.draw_networkx(albero, pos)
+        plt.pause(2)
+        plt.axes().clear()
 
     return albero
-
-
-grafo_test = nx.Graph()
-grafo_test.add_nodes_from('ABCDEFGHILMN')
-grafo_test.add_edges_from(('AB', 'BC', 'CD', 'DE', 'EF', 'FG', 'GH', 'HI', 'IL', 'LA', 'MA', 'MD', 'MN', 'BL', 'CI', 'IG', 'NF', 'NH'))
-
-albero_ricoprimento1 = visita_ampiezza(grafo_test, 'A', True, True)
-albero_ricoprimento2 = visita_profondita(grafo_test, 'A', True, True)
-print(albero_ricoprimento1.nodes)
-print(albero_ricoprimento1.edges)
-print(albero_ricoprimento2.nodes)
-print(albero_ricoprimento2.edges)
-print(grafo_test.nodes)
-print(grafo_test.edges)
