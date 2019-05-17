0aadf2583a2c2b2851220da5aa8e0742d095b8ff
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-05-17 16:48:28 +0200

    Update grafo_test.py

--- grafo_test.py
+++ grafo_test.py
@@ -1,12 +1,20 @@
+import esercizio_pcs as es
 import networkx as nx
 
-# Ricostruiamo il grafo delle slide per testare le funzione
+# Ricostruiamo il grafo delle slide per testare le funzioni
+
 grafo_test = nx.Graph()
 grafo_test.add_nodes_from('ABCDEFGHILMN')
-grafo_test.add_edges_from(('AB', 'BC', 'CD', 'DE', 'EF', 'FG', 'GH', 'HI', 'IL', 'LA', 'MA', 'MD', 'MN', 'BL', 'CI', 'IG', 'NF', 'NH'))
+grafo_test.add_edges_from(('AB', 'BC', 'CD', 'DE', 'EF', 'FG', 'GH', 'HI', 'IL',
+                           'LA', 'MA', 'MD', 'MN', 'BL', 'CI', 'IG', 'NF', 'NH'))
 
-# Come modificare attributi di un grafo
-petersen = nx.petersen_graph()
-petersen.nodes[0]['colore'] = 'rosso'
-petersen.nodes[0]['valore'] = 1.5
-petersen.nodes[0] # mi dà attributo e valore
+albero_ricoprimento1 = es.visita_ampiezza(grafo_test, 'A', plot_albero=True, plot_grafo=True, plot_anim=True)
+albero_ricoprimento2 = es.visita_profondita(grafo_test, 'A', plot_albero=True, plot_grafo=True, plot_anim=True)
+
+# print(albero_ricoprimento1.nodes)
+# print(albero_ricoprimento1.edges)
+# print(albero_ricoprimento2.nodes)
+# print(albero_ricoprimento2.edges)
+# print(grafo_test.nodes)
+# print(grafo_test.edges)
+
