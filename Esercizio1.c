9bca08ebffd9ee79eb5f4b22fbcba0d10b7e76fc
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-06-14 17:35:01 +0200

    Update Esercizio1.c

--- Esercizio1.c
+++ Esercizio1.c
@@ -1,58 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-//definisco i prototipi:
-char* swap(char *vett,int N);
+
+#define VERBOSITY 2
+
+char* swap(char *vett, int N);
 
 int main(){
-int N,i;
-char *vett=NULL;
-printf("Inserisci la lunghezza del vettore:\n");
-scanf("%d",&N);
-vett=(char*)malloc(N*sizeof(char));
+    int N, i;
+    char *vett = NULL;
 
-//faccio il controllo se l'allocazione è andata a buon fine:
-if (vett == NULL) {
-  fprintf(stderr, "la malloc ha fallito\n");
-  return(-1);
-}
+    printf("Inserisci la lunghezza del vettore:\n");
+    scanf("%d", &N);
+    vett = (char*)malloc(N*sizeof(char));
 
-//Qui memorizzo gli elementi dinamicamente in un vettore:
-printf("Inserisci ora i numeri che vuoi inserire:\n");
-for(i=0;i<N;i++)
-{
-    
-    scanf(" %c", &vett[i]);
-}
-printf("Il vettore che hai stampato e':\n");
-for(i=0;i<N;i++) { 
-    printf("v[%d]=%c\n",i,vett[i]); 
+    if (vett == NULL)
+    {
+      fprintf(stderr, "\nLa malloc ha fallito\n");
+      return(-1);
     }
 
+    printf("\nInserisci ora i caratteri:\n");
+    for(i=0; i<N; i++)
+    {
+        scanf(" %c", &vett[i]);
+    }
 
-//Uso la funzione SWAP
-swap(vett,N);
+    #if VERBOSITY >= 1
+    printf("\nCaratteri inseriti: \n");
+    for(i=0; i<N; i++)
+    {
+        printf("%c", vett[i]);
+    }
+    printf("\n");
+    #endif
 
-printf("\n\nIl vettore dopo la trasformazione e':\n");
-for(i=0;i<N;i++)
-{
-    printf("%c",vett[i]);
+    swap(vett,N);
+
+    printf("\n\nIl vettore dopo la trasformazione e':\n");
+    for(i=0;i<N;i++)
+    {
+        printf("%c",vett[i]);
+    }
+    return 0;
 }
-return 0;
-}
 
-
-//Qui eseguo lo swap:
-//Io restituisco un puntatore a un int (un vettore di interi)
 char* swap(char vett[],int N){
-int i;
-char tmp;
 
-for(i=0;i<(N/2);i++){
-    tmp=vett[i];
-    vett[i]=vett[N-i-1];
-    vett[N-i-1]=tmp;
-    }
+    int i;
+    char tmp;
+
+    for(i=0;i<(N/2);i++)
+        {
+            tmp = vett[i];
+            vett[i] = vett[N-i-1];
+            vett[N-i-1] = tmp;
+            #if VERBOSITY >= 2
+            printf("\nScambiati i caratteri: %c %c", vett[N-i-1], vett[i]);
+            #endif
+        }
+
     return vett;
 }
-
-
