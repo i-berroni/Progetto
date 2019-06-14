9e1ea715ac8f76ca1f094bf7f0eb6f1a9124e6bd
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-06-14 17:27:33 +0200

    Update Esercizio0_finale.c

--- Esercizio0_finale.c
+++ Esercizio0_finale.c
@@ -1,52 +1,51 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VERBOSITY 2
+
 int main(){
-int N,i;
-char *vett=NULL,tmp;
+    int N, i;
+    char *vett = NULL, tmp;
 
-printf("Inserisci la lunghezza del vettore:\n");
-scanf("%d",&N);
-vett=(char*)malloc(N*sizeof(char));
+    printf("Inserisci la lunghezza del vettore:\n");
+    scanf("%d", &N);
+    vett = (char*)malloc(N*sizeof(char));
 
-//faccio il controllo se l'allocazione è andata a buon fine:
-if (vett == NULL) {
-  fprintf(stderr, "la malloc ha fallito\n");
-  return(-1);
-}
-
-
-printf("Inserisci ora i caratteri che vuoi inserire:\n");
-for(i=0;i<N;i++)
-{
-    scanf(" %c", &vett[i]);
-    
-}
-printf("Il vettore che hai stampato e':\n");
-for(i=0;i<N;i++) { 
-    printf("v[%d]=%c\n",i,vett[i]); 
+    if (vett == NULL)
+    {
+      fprintf(stderr, "\nLa malloc ha fallito\n");
+      return(-1);
     }
 
-for(i=0;i<(N/2);i++){
-    tmp=vett[i];
-    vett[i]=vett[N-i-1];
-    vett[N-i-1]=tmp;
+    printf("\nInserisci ora i caratteri:\n");
+    for(i=0; i<N; i++)
+    {
+        scanf(" %c", &vett[i]);
+    }
+
+    #if VERBOSITY >= 1
+    printf("\nCaratteri inseriti: \n");
+    for(i=0; i<N; i++)
+    {
+        printf("%c", vett[i]);
+    }
+    printf("\n");
+    #endif
+
+    for(i=0; i<(N/2); i++)
+    {
+        tmp = vett[i];
+        vett[i] = vett[N-i-1];
+        vett[N-i-1] = tmp;
+        #if VERBOSITY >= 2
+        printf("\nScambiati i caratteri: %c %c", vett[N-i-1], vett[i]);
+        #endif
+    }
+    printf("\n\nIl vettore dopo la trasformazione e':\n");
+    for(i=0;i<N;i++)
+    {
+        printf("%c",vett[i]);
+    }
+    return 0;
+
 }
-printf("\n\nIl vettore dopo la trasformazione e':\n");
-for(i=0;i<N;i++)
-{
-    printf("%c",vett[i]);
-}
-return 0;
-
-
-}
-
-
-
-
-
-
-
-
