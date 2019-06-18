a778dcbae8cf84bb991ab51f897a3be56a3b7a87
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-06-18 09:48:24 +0200

    Update grafo_test.py

--- grafo_test.py
+++ grafo_test.py
@@ -1,7 +1,7 @@
 import esercizio_pcs as es
 import networkx as nx
 
-# Ricostruiamo il grafo delle slide per testare le funzioni
+# Si ricostruisce il grafo delle slide per testare le funzioni
 
 grafo_test = nx.Graph()
 grafo_test.add_nodes_from('ABCDEFGHILMN')
@@ -10,11 +10,3 @@ grafo_test.add_edges_from(('AB', 'BC', 'CD', 'DE', 'EF', 'FG', 'GH', 'HI', 'IL',
 
 albero_ricoprimento1 = es.visita_ampiezza(grafo_test, 'A', plot_albero=True, plot_grafo=True, plot_anim=True)
 albero_ricoprimento2 = es.visita_profondita(grafo_test, 'A', plot_albero=True, plot_grafo=True, plot_anim=True)
-
-# print(albero_ricoprimento1.nodes)
-# print(albero_ricoprimento1.edges)
-# print(albero_ricoprimento2.nodes)
-# print(albero_ricoprimento2.edges)
-# print(grafo_test.nodes)
-# print(grafo_test.edges)
-
