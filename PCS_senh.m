41508b49605d148c2aac4bb964b66a0242c90642
robymark97 <48909209+robymark97@users.noreply.github.com>
2019-06-18 09:12:56 +0200

    Update PCS_senh.m

--- PCS_senh.m
+++ PCS_senh.m
@@ -1,4 +1,4 @@
-%esercizio_6_con_esercizio_7_aritmetica_finita
+% Esercizi 6 e 7 Aritmetica finita
 clear all
 close all
 clc
@@ -6,40 +6,46 @@ format long e
 
 x=10.^(-(0:12));
 x=x';
-%seno iperbolico 1
+
+% Seno iperbolico 1
 y1 = senh1(x);
-%seno iperbolico 2
+
+% Seno iperbolico 2
 y2=zeros(length(x),1);
 for i=1:length(x)
-y2(i) = senh2(x(i));
+    y2(i) = senh2(x(i));
 end
-%valore implementato in matlab del seno iperbolico:
+
+% Valore implementato in matlab del seno iperbolico:
 y_matlab=sinh(x);
-%preallocazioni degli errori:
+
+% Preallocazioni degli errori:
 err1 = zeros(length(x),1);
 err2 = zeros(length(x),1);
-%errori relativi:
+err1abs = zeros(length(x),1);
+err2abs = zeros(length(x),1);
+
+% Errori relativi:
 err1(1:13)=abs(y1(1:13)-y_matlab(1:13))./abs(y_matlab(1:13));
 err2(1:13)=abs(y2(1:13)-y_matlab(1:13))./abs(y_matlab(1:13));
 
-
-figure;
+figure(1);
 loglog(x,err1,'-o','MarkerIndices',1:length(x));
-figure;
+legend('Errore relativo di senh1')
+figure(2);
 loglog(x,err2,'-o','MarkerIndices',1:length(x));
+legend('Errore relativo di senh2')
 
-
-
-%errori assoluti:
+% Errori assoluti:
 err1abs(1:13)=abs(y1(1:13)-y_matlab(1:13));
 err2abs(1:13)=abs(y2(1:13)-y_matlab(1:13));
 
-figure;
+figure(3);
 loglog(x,err1abs,'-o','MarkerIndices',1:length(x));
-figure;
+legend('Errore assoluto di senh1')
+figure(4);
 loglog(x,err2abs,'-o','MarkerIndices',1:length(x));
-
-
+legend('Errore assoluto di senh2')
 
 
 
